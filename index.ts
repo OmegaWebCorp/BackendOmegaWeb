1c44d5ee7d96d7d2bea3e4356da6a14d87ad2773
Jeferson <guevarajeferson@gmail.com>
2021-12-01 00:05:09 -0500

    Confirmación de configuraciones para conexión con la BD para consultar y crear de acuerdo a los tipos de datos

--- index.ts
+++ index.ts
@@ -1,8 +1,29 @@
-import conectarBD from "./db/db";
+import conectarBD from './db/db';
+import { UserModel } from './models/users';
+
+const main = async () => {
+    await conectarBD();
 
 
-const main =async()=>{
-await conectarBD();
+    // await UserModel.create({
+    //     correo: 'prueba@prueba.com',
+    //     identificacion:'123456',
+    //     nombre:'prueba',
+    //     apellido:'prueba   apellido'
+    // }).then((u)=>{
+    //     console.log('Usuario Insertado', u);
 
+    // }).catch((e)=>{
+    //     console.log('Error insertando usuario', e);
+
+    // });
+
+    await UserModel.find()
+        .then((u) => {
+            console.log('Consulta exitosa todos los usuarios', u);
+
+        }).catch((e) => {
+            console.log('error consultando usaurios', e);
+        });
 };
 main();
